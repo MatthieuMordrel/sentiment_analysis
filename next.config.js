37695c32b0fd524907c2341c834155ce5f737fc9
MatthieuMordrel <matthieumordrel@gmail.com>
2024-06-05 11:56:19 +0200

    More Testgin

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination:
-          process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : '/api/',
+          process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000/api/:path*'
+            : '/api/:path*',
       },
       {
         source: '/test',
