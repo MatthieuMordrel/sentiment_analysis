51d3dc0b2e59ab8299839973ca30608eeaa7d7da
MatthieuMordrel <matthieumordrel@gmail.com>
2024-06-05 11:50:06 +0200

    Testing rewriting

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : '/api/',
       },
+      {
+        source: '/test*',
+        destination:
+          process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/:path*' : '/',
+      },
     ];
   },
 };
