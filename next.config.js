d190c4a91916da795293c2db3d85ab71f2e65905
MatthieuMordrel <matthieumordrel@gmail.com>
2024-06-18 18:12:44 +0200

    Test with no config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/python',
-        destination:
-          process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/python' : '/api',
-      },
-    ];
-  },
+  // rewrites: async () => {
+  //   return [
+  //     {
+  //       // source: '/api/python',
+  //       // destination:
+  //       //   process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/api/python' : '/api',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
