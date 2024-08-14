67217f91cdd85ccddbf4157b61e8890baf762263
MatthieuMordrel <matthieumordrel@gmail.com>
2024-08-14 12:29:58 +0200

    minor improvements & syntax fixes

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface Article {
@@ -24,20 +25,38 @@ const Card: React.FC<CardProps> = ({ article, onHoverContent, isHighlighted }) =
     year: 'numeric',
   });
 
-  const sentimentLabel = article.sentiment > 0.5 ? 'Very good' : article.sentiment > 0.2 ? 'Good' : article.sentiment < -0.2 ? 'Bad' : 'Neutral';
-  const sentimentColor = article.sentiment > 0.2 ? 'text-green-500' : article.sentiment < -0.2 ? 'text-red-500' : 'text-gray-700';
+  const sentimentLabel =
+    article.sentiment > 0.5
+      ? 'Very good'
+      : article.sentiment > 0.2
+      ? 'Good'
+      : article.sentiment < -0.2
+      ? 'Bad'
+      : 'Neutral';
+  const sentimentColor =
+    article.sentiment > 0.2
+      ? 'text-green-500'
+      : article.sentiment < -0.2
+      ? 'text-red-500'
+      : 'text-gray-700';
 
   return (
     <div
-      className={`flex p-4 mb-4 border rounded shadow-sm ${isHighlighted ? 'bg-blue-100' : 'bg-white'}`}
+      className={`flex p-4 mb-4 border rounded shadow-sm ${
+        isHighlighted ? 'bg-blue-100' : 'bg-white'
+      }`}
       onMouseEnter={() => onHoverContent({ url: article.url, content: article.content })}>
-      <img src={article.urlToImage} alt={article.title} className='w-32 h-32 object-cover mr-4' />
-      <div className='flex flex-col justify-center' style={{ width: 'calc(100% - 8rem)' }}>
+      <Image src={article.urlToImage} alt={article.title} className='w-32 h-32 object-cover mr-4' />
+      <div className='flex flex-col justify-center w-[calc(100%-8rem)]'>
         <p className='text-gray-500 mb-2'>
           {formattedDate} - {article.source.name}
         </p>
         <h2 className='text-xl font-bold mb-2 text-black'>
-          <a href={article.url} target='_blank' rel='noopener noreferrer' className='hover:underline hover:text-blue-500'>
+          <a
+            href={article.url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:underline hover:text-blue-500'>
             {article.title} -
           </a>
           <span className={`ml-2 ${sentimentColor}`}>Sentiment: {sentimentLabel}</span>
